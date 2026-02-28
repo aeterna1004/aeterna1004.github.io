8cf6b1a4aaa5440860c9fca635ff31dd9541999f
Nguyễn Nhật Trường <nhattruong16062003@gmail.com>
2026-02-28 10:32:47 +0700

    Updates

---
+++ _next/static/HnZm-KlPZkK6oMPlE3zOR/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/aeterna1004.github.io//_next/:path+",
+        "destination": "/aeterna1004.github.io/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
